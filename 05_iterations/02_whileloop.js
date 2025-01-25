db7b750474427f6e8620e10b2e98fb7624408d07
Krishnapal singh Chouhan <104709843+kpsingh21@users.noreply.github.com>
2025-01-25 20:26:18 +0000

    while do-while loops

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,27 @@
+//while loop execute when condition is true
+// while (condition) { }
+
+let index = 0
+while (index <= 10){
+    // console.log(`value of index is ${index} `);
+    index = index+2
+}
+
+let myArray = ["a","b","c"]
+let arr =0;
+while (arr< myArray.length){
+    // console.log(`value is ${myArray[arr]}`)
+    arr++; // arr = arr+1;
+}
+
+
+//do while loop : this loop execute atleat one always
+// do {  
+// } while (condition);
+
+// let score = 1
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
