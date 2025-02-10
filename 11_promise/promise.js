d5b273e27cb0ac8566a07f51fd95628698733a65
Krishnapal singh Chouhan <104709843+kpsingh21@users.noreply.github.com>
2025-02-10 19:05:08 +0000

    Promises In JS

--- 11_promise/promise.js
+++ 11_promise/promise.js
@@ -75,3 +75,42 @@ promiseFour
 }).catch(function(error){
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected"))
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+
+
+//try catch syntax
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
+    }
+}
+
+getAllUsers()
+
+
+//.then .catch  syntax
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
